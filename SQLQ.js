0fa16daca8a2a5a6575ad16e8d100425ab3c5bb1
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-06 12:53:08 -0600

    add

---
+++ SQLQ.js
@@ -0,0 +1,89 @@
+//1. Working with our design team and clients to define website, application
+// and data requirements.
+
+//A: In my class we stuck to agile methodology and ultimately reaching the MVP of the project.
+//What does this application specifically need to do? / Test my product
+// Correct as needed. And work until MVP is complete, once it is- the team can continue
+//to add features as needed.
+
+//2. Developing web applications and web services using a variety of toolsets
+//including:
+//a: ASP: Server-Side scripting language and engine for dynamic web pages
+//typically used for the back end of an application. Created by microsoft.
+
+//b: ASP.NET: is server side web app framework to build dynamic web pages.
+//It was the first veresion of the web-adapted .net framework. ASP.NET CORE 
+//is a new and improved, like .NET Framework. Based on HTTP Protocol for req, res
+//and communication, supports multiple languages, reusable components, language runtime
+// security access, less code. built in HTML generator, security alerts , built in 
+//caching system. 
+
+//c: C# - C SHARP
+// used with framework .net, use of CLR is a translation application. 
+//an object oriented programming, build with components
+
+//d: HTML
+//standard markup language for documents designed to be displayed in the web browser.
+//Where you use tags in order to render img, words, videos, etc on the page
+//html can have css added or in the use of style tags as well as js in script tags. 
+
+//e: JavaScript: I am very comfortable with writing in Javascript. Within my 16 week course,
+// we were using JS for 14 weeks of it. We worked on algorithms, big O notation, web design, in both
+//group and solo situations. I like javascript because it is a very popular language, therefore,
+//when you're in a bind, stackoverflow or a great tutorial to learn how to solve that particular issue.
+//I also enjoy it because I love React and since React works with JSX, its a big win. 
+
+//f. CSS
+// I am very comfortable with CSS, it was essential in my class to not only learn how to use it well
+// but to also make sure that our design is responsive. Since most users are mobile users, its essential
+//to make sure its functional on both desktop, mobile of all varieties and everything in between.
+
+
+//3. Developing T-SQL queries, views and stored procedures, against MS SQL
+//server databases.
+
+//It's a declarative language  which is used to communicate to the database to instruct
+//what operation you want ot peform agains the database like select or update.
+//3 major types of TSQL statements. 1. Data Maniupulation Language which isused
+//to modify and query. This includes, select to fetch data and INSERT UPDATE and DELETE.
+//2. Data Definition Language, is the set of T-SQL statments that aure used to define db objects with
+//statesment like create, alter and drop
+//3. Data Control Language used for security permissions with statements such as 
+//GRANT, REVOKE and DENY.
+
+//4. Developing and updating reports using MS SQL Server Reporting Services.
+//1. to update you download the exisiting report, edit in the BIDS and save
+//then click report file.
+
+//5. Providing excellent technical support to our internal and external clients.
+//In terms of customer service, I have a lot of experience. For over two years I was responsible 
+// for sales, response to feedback, providing solutions (especially during covid) and team management.
+//In terms of strictly technical support, I was able to foster that skill as well within my bootcamp.
+//We worked on a lot of group projects and there was a peer that struggled with the material additionally,
+// hes bilingual, with his native language as mandarin. I continued to help on a technical side as
+// well as an encouragement that he was capable. 
+
+//6. Develop complex queries including multi-join tables, aggreagate functions, unions,
+//and sub-queries.
+
+//7.Query Analyzer:
+//  SQL Profiler:
+//  Reporting Services:
+// Integration Services:
+
+
+//8: Knowledge of basic TCP/IP addressing and DNS:
+
+//9: ASP, PHP, CSS and JS:
+
+
+//10. XML and HTML:
+
+//11. Speak about your organization, attention to detail, sense of urgency,
+//ability to follow through, handle multiple requests and meet deadline.
+
+// 12. Speak about your analytical and problem solving skills
+
+// 13. Ability to work independently, while responding to requests from supervisors, etc.
+
+// 14. Flexible and willing to adapt.
---
+++ SQLquestions.js
@@ -0,0 +1,109 @@
+//TOP SQL interview questions
+
+//1. Tell me about yourself and why you want this position
+// I am a reliable team player who you can rely on to always bring
+//professionalism and passion into my career. I am goal oriented and will ensure
+// company goals are met. My level of technicality are a strong match for the role
+//based on my certification in both front end and back end dev. I have experience working
+//with other developers and I believe that I can contribute to the team positively. 
+
+//2. What is SQL?
+// Structured Query Language, more commonly known as SQL, is seen as the standard for 
+// managing data kept in relational database management systems. update data and retrieve
+//specific data
+
+//3. Why our company in this position?
+//Based on the reviews, based on my passion for development and my eagerness to learn
+//Also, after reviewing the company portfolio, I was impressed with the designs and (BOKA health)
+// would be honored to say I contributed to a project in the future. 
+
+//4. What is MySQL?
+// MySQL is an open-source relational database management system that is based on SQL, 
+//most commonly used for web databases. It was co-founded by Michael Widenius.
+// Used in WordPress, YouTube, facebook and twitter. written in C and C++ languages
+
+//5. What's the main difference between SQL and MySQL?
+// SQL is often used to access, manipulate and update the data within a database, whereas MySQL
+// is a relational database management system that keeps the data within db organized.
+// SQL is a language created to manage realtion databses, and MySql is an open source
+//relational database management system based on SQL.
+
+//6. In SQL, what are "JOINS"?
+// There are four main types. FULL, RIGHT, LEFT and INNER. combine the rows from two or more tables
+
+//7. What is an INDEX, and why is it useful to have?
+// An index is useful because it allows for faster retrieval of records from a table.
+//The index creates an entry for each value, which in turn makes it much faster. An 
+//inded is a pointer to data contained within a table.
+
+//8. What is a 'constraint' in SQL?
+// There are many differnt types of SQL constraints. Their purpose is to specify
+// a rule for the data within a table, limiting the type of data that can go within the table.
+
+//9. Name some constraints and what they do?
+//Primary Key: which identifies each record in a table
+//NOT NULL: is used o prevent a NULL value being inserted
+//FOREIGN KEY: which provides a unique identification of a row or record within another table
+//DEFAULT:  which will assign a deafault value if not already specified.
+//CHECK: verify that all values meet criteria
+//UNIQUE: verify that all values are unique
+
+//10. what are tables and fields?
+//table is a collection of data that is organized into rows and columns
+// columns are called "fields"
+
+//11. Biggest Weakness?
+// Healthy Work/Life balance
+
+//12. Different subsets of SQL?
+// Data Control Language: permits you to control access to db
+// Data Definition Language: allow you to specify data structures in db, delete and altering
+// Data Manipulation Language: allows you to maninpute data like update, delete, insert
+
+//13. How would you format SQL server dates?
+//Format function, allows the formatting of the date and time
+// select format (getdate(), 'dd/mm/yyyy')
+
+//14. What is primary and foreign key?
+// For a db table to qualify as 'relational', it needs to have a primary key. A PK has one
+//or more columns. used to identify each row in a table. cannot have null values
+//a FK is a column in one table. fk can accept null values.
+
+//15. What is database denormalization?
+// Denormalization is a method used to improve the performance of a db,
+// allowing the retrieval of data in the quickest time possible.
+
+//16. What is database normalization?
+//  normalization reduces redundancy of data within a database. Normalization is a design
+// technique that ;laces larger db tables into smaller tables and then uses specific relationships
+// to link them. downside, could be slower.
+
+//17. In SQL, what is a subquery?
+// inner query or inner select. its an sql query thats nested inside a larger query,
+// such as SELECT, update, insert
+// like when you are selecting tow different column names from seperate tables 
+
+//18. What happens to data rows in a table when the table contains a clustered index?
+// will be stored in order. Cluster indexes sort data rows based on their key values.
+//if not used, it would be a heap. 
+
+//19. Aggrogate Functions:
+// SELECT AVG(release_year) FROM albums;
+//all the data returned from the select and then returns a function on it.
+// avg, sum, count, 
+// count is useful when 
+
+//given alias name to new column names and selected columns
+// SELECT b.name AS band_name, COUNT(a.id) AS num_albums 
+//FROM bands AS b
+//left join, getting bands with no albums records as well
+//LEFT JOIN albums AS a ON b.id = a.band_id
+// grouping them by band.id
+//GROUP BY b.id;
+
+// 20. How to query on aggrogated data?
+// "HAVING" underneath the group by;
+
+
+// SELECT COUNT(*) from table
+//
