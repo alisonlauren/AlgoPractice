535d4ca50aafac2dd2347d61b1f34ec96354da86
Alison Manning <alisonlaurenmanning@gmail.com>
2021-03-25 14:26:27 -0500

    changed slightly, tiny bug fixed.

--- rethink.py
+++ rethink.py
@@ -1,17 +1,14 @@
 
-bad_words = ["mean", "ugly" "I hate", "bad", "wrong", "cry", "worse"]
+bad_words = ["mean", "ugly", "I hate"]
 user_input = input("Send a Text? ")
 question_parts = user_input.split(" ")
 for part in question_parts:
     if part.lower() in bad_words:
-        user_choice = input("are you sure you want to send this text? Type Y or N ")
+        user_choice = input("are you sure you want to send this text? It seems hurtful. Type Y or N? ")
         if user_choice == "Y":
             print("wow ok, text is sent")
         elif user_choice == "N":
             print("good choice, text deleted")
-for part in question_parts:
-    if part.lower() not in bad_words:
-        print("Great text!")
 
 
     
