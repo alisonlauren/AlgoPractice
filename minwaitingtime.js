7371646c0dab2cf12912b085d31c3c4d91852ce2
Alison Manning <alisonlaurenmanning@gmail.com>
2021-03-29 10:47:11 -0500

    yay more algo's

---
+++ minwaitingtime.js
@@ -0,0 +1,16 @@
+let waitTime = [5,6,7,1,2,33]
+
+function minimumWaitingTime(queries) {
+    queries.sort((a, b) => a - b)
+
+    let totalWaitingTime = 0;
+    for (let index = 0; index < queries.length; index++) {
+        const duration = queries[index];
+        const queriesLeft = queries.length - (index + 1);
+        totalWaitingTime += duration * queriesLeft;
+    }
+    return totalWaitingTime;
+}
+
+console.log(minimumWaitingTime(waitTime))
+
---
+++ nonconst.js
@@ -0,0 +1,24 @@
+function nonConstructibleChange(coins) {
+    // probs going to be a new array here
+    //definitely a for loop
+    // this loop we want to manipulate so that it adds
+    //every combination that the coins can create in sums
+    // once the computer has run that, we want to find the smallest
+    //quantitiy that is NOT in the sumCoins array.
+    //and if i have no coins, just return the number one
+    return 1;
+}
+
+let myCoins = [1,2,5]
+
+function nonConstructibleChange(coins) {
+    coins.sort((a, b) => a - b);
+    let currentChangeCreated = 0;
+    for (const coin of coins) {
+        if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+        currentChangeCreated += coin;
+    }
+    return currentChangeCreated + 1;
+}
+
+console.log(nonConstructibleChange(myCoins))
