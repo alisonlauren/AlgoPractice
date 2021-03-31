8f0159c2d0fa0fb618aeb844dac8fb84219175eb
Alison Manning <alisonlaurenmanning@gmail.com>
2021-03-31 12:39:45 -0500

    extra practice with logic

--- arraySq.js
+++ arraySq.js
@@ -1,15 +1,54 @@
-let numbers = [2,4,6]
+// let numbers = [2,4,6]
+
+// function sortedSquaredArray(array) {
+//     let squaredArray = []
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+//         let newIndex = element * element;
+//         squaredArray.push(newIndex);
+//     }
+//     squaredArray.sort((a,b) => a - b);
+//     return squaredArray;
+// }
+
+// console.log(sortedSquaredArray(numbers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const myNumbers = [2,3,4]
+const myNumbersUnordered = [4,3,2]
 
 function sortedSquaredArray(array) {
-    let squaredArray = []
+    let newArray = []
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        let newIndex = element * element;
-        squaredArray.push(newIndex);
+        let squaredNumber =  element * element;
+        newArray.push(squaredNumber);
+        
     }
-    squaredArray.sort((a,b) => a - b);
-    return squaredArray;
+    newArray.sort((a,b) => a - b);
+    return newArray
 }
 
-console.log(sortedSquaredArray(numbers))
+console.log(sortedSquaredArray(myNumbers))
+console.log(sortedSquaredArray(myNumbersUnordered))
 
